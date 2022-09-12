5e341d3f17ce4f136ce0af9aa748ef8d7adf7800
root <root@kali.azatezalo>
2022-09-11 21:20:12 -0400

    Client File Comments Added.

--- server.py
+++ server.py
@@ -3,3 +3,13 @@ Description: The server responsible for acting as our backdoor.
 Author: Aleksa Zatezalo
 Date: September 2022
 """
+
+# Imports
+
+
+# Setting Up IP/Sockets
+
+
+# Initializing Connection
+
+# Runtime Loop
